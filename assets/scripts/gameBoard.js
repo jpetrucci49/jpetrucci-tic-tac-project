774714c754603d66c4c97f7d493f6811823af7d1
jpetrucci <jpetrucci49@yahoo.com>
2018-08-10 10:11:56 -0400

    Add turn tracker
    X is first player. After O plays valid spot, O plays.
    If DRAW, wait 1 second, clear board to replay.

--- assets/scripts/gameBoard.js
+++ assets/scripts/gameBoard.js
@@ -2,19 +2,34 @@
 let gameWin = false
 let played = []
 const winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+let x = true
 
+const clearBoard = function () {
+  $('div.box').text('')
+}
 const ticTacBoard = function (e) {
-  const cell = parseInt(e.target.id)
+  const tgt = e.target
+  const cell = parseInt(tgt.id)
   console.log('You clicked cell -', cell)
   if (played.includes(cell)) {
     console.log('That cell is already played.')
   } else {
+    if (x) {
+      $(tgt).text('X')
+      x = !x
+    } else {
+      $(tgt).text('O')
+      x = !x
+    }
     played.push(cell)
   }
 
   if (played.length === 9 && !gameWin) {
     console.log('Game over, It\'s a draw.')
     played = []
+    x = true
+
+    setTimeout(clearBoard, 1000)
   }
   console.log('Cells played -', played)
 }
