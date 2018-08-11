b6e736d245fe9198d6d94d33ad069f3688452b75
jpetrucci <jpetrucci49@yahoo.com>
2018-08-10 23:22:59 -0400

    Update cell match
    Derping around with cell text detection for `if` condition.

--- assets/scripts/gameBoard.js
+++ assets/scripts/gameBoard.js
@@ -7,6 +7,32 @@ let x = true
 const clearBoard = function () {
   $('div.box').text('')
 }
+
+const match = function () {
+  // if (played.length < 3) {
+  //   return false
+  // }
+  const result = winConditions.some(function (condition) { // reduce(function(a, b){ return (a === b) ? a : NaN; })
+    if (condition.every(id => played.includes(id)) condition.every(y => $(`div#${id}`).text())) {
+      console.log('Yeaaaaaaah')
+    }
+  })
+  return result
+  // winConditions.forEach(function (x) {
+  //   return played.forEach(function (y) {
+  //     console.log((x.indexOf(y) > -1))
+  // })
+  // })
+}
+
+// var array1 = [[1, 3, 5], [2, 4, 7], [1, 5, 9]],
+//     array2 = [1, 2, 4, 5, 9],
+//     result = array1.some(a => a.every(v => array2.includes(v)));
+
+const winCheck = function () {
+  console.log(match())
+}
+
 const ticTacBoard = function (e) {
   const tgt = e.target
   const cell = parseInt(tgt.id)
@@ -22,6 +48,7 @@ const ticTacBoard = function (e) {
       x = !x
     }
     played.push(cell)
+    winCheck()
   }
 
   if (played.length === 9 && !gameWin) {
