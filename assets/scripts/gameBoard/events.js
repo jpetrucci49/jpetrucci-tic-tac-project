ce2661b05ca097791d28f26a71f113bb0734c1e6
jpetrucci <jpetrucci49@yahoo.com>
2018-08-14 17:19:49 -0400

    Remove unused functions
    winConditions removed
    onTicTac removed
    clearBoard removed

--- assets/scripts/gameBoard/events.js
+++ assets/scripts/gameBoard/events.js
@@ -3,13 +3,7 @@ const api = require('./api.js')
 const ui = require('./ui.js')
 const store = require('./../store.js')
 
-const winConditions = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']]
 let player
-
-const clearBoard = function () {
-  x = true
-  $('div.box').text('')
-}
 
 const currentPlayer = function () {
   if (store.x) {
@@ -20,54 +14,13 @@ const currentPlayer = function () {
   store.x = !store.x
   return player
 }
-const checkWin = function () {
-  const result = winConditions.some(function (condition) {
-    //  If line is filled
-    if (condition.every(id => store.played.includes(id))) {
-      const line = []
-      condition.forEach(id => {
-        line.push($(`div#${id}`).text())
-      })
-      const test = line.every(x => line.every(y => x === y))
-      if (test) {
-        store.game.over = true
-      }
-    }
-  })
-  return result
-}
 
-const ticTacBoard = function (e) {
-  if (gameWin === false) {
-    const tgt = e.target
-    const cell = parseInt(tgt.id)
-    if (played.includes(cell)) {
-    } else {
-      if (x) {
-        $(tgt).text('X')
-        x = !x
-      } else {
-        $(tgt).text('O')
-        x = !x
-      }
-      played.push(cell)
-      store.played.push(cell)
-      checkWin()
-    }
-
-    if (played.length === 9 && !gameWin) {
-      $('#message').text(`Too bad, It's a draw.`)
-    } else if (gameWin) {
-      const winner = $(`div#${played[played.length - 1]}`).text()
-      $('#message').text(`${winner} is the Winner!`)
-    }
-  }
-}
 const onNewGame = function () {
   api.newGame()
     .then(ui.newGameSuccess)
     .catch(ui.newGameFail)
 }
+
 const onPlacePiece = function (e) {
   if (store.game.over === false && store.played.length < 9) {
     if (store.played.includes(e.target.id)) {
@@ -95,12 +48,14 @@ const onPlacePiece = function (e) {
     }
   }
 }
+
 const onListGame = function (e) {
   e.preventDefault()
   api.listGame()
     .then(ui.listGameSuccess)
     .catch(ui.listGameFail)
 }
+
 const handler = function () {
   $('#ticTac > div').on('click', onPlacePiece)
   $('#new-game-shell').on('click', onNewGame)
